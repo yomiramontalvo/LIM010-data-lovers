0114670d1330a4067763bc757581f9892d9420cd
Thanet0109 <thanet0109@gmail.com>
2019-06-27 17:52:48 -0500

    mandando funcionalidad de js primera historia

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ enter.addEventListener('click', () => {
     const write = document.getElementById('write').value;
     const pass = document.getElementById('pass').value;
     if (write == "LABORATORIA" && pass == "LABORATORIA") {
-        document.getElementById('error1').innerHTML = "Cool!!";
+        document.getElementById('error').innerHTML = "Cool!!";
     }
     else {
         document.getElementById('error').innerHTML = "Registra bien tu usuario y contraseña"
