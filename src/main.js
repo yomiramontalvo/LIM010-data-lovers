d588c1037b7923d06e4752ac17391836e1a1c90b
Thanet0109 <thanet0109@gmail.com>
2019-06-27 11:36:32 -0500

    completando el JS de la Primera Historia

--- src/main.js
+++ src/main.js
@@ -1,34 +1,12 @@
 /* Manejo del DOM */
-const ingresar = document.getElementById("ingresar");
-const escribe = document.getElementById("escribe");
-ingresar.addEventListener('click',() =>{
-    const escribe= document.getElementById("escribe")
-    if (escribe == "LABORATORIA"){
-        document.getElementById("error").innerHTML="Bienvenido pudiste ingresar sin problemas" } });
-
-
-/*function dar_click(){
-    const n = llamar.value;
-    alert("Hola " + n +" ! Haz clic en inicio");
-  }
-
-  const verificar = document.getElementById("verificar");
-let intentos = 0;
-verificar.addEventListener('click', () => {
-  const contraseña = document.getElementById("pass").value;
-  if (contraseña == "LABORATORIA") {
-    document.getElementById("pantalla1").classList.add("hide");
-    document.getElementById("pantalla2").classList.remove("hide");
-  }
-  else {
-    intentos = intentos + 1;
-    document.getElementById("falla").innerHTML = "Ups! Contraseña incorrecta, intente otra vez";
-
-
-    if (intentos == 3) {
-      document.getElementById("falla").innerHTML = "Ya utilizaste todos tus intentos, en este momento no podrás ingresar";
-      document.getElementById('verificar').disabled = true;
-      document.getElementById("pass").disabled = true;
+const enter = document.getElementById('enter');
+enter.addEventListener('click', () => {
+    const write = document.getElementById('write').value;
+    const pass = document.getElementById('pass').value;
+    if (write == "LABORATORIA" && pass == "LABORATORIA") {
+        document.getElementById('error1').innerHTML = "Cool!!";
     }
-  }
-}); */
+    else {
+        document.getElementById('error').innerHTML = "Registra bien tu usuario y contraseña"
+    }
+});
